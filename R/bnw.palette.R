70e3c2b34625e00391db4d61e0625f685cb44abb
enzo03gomes <enzo03gomes@gmail.com>
2025-03-05 20:47:33 +0100

    added bnw.palette

---
+++ R/bnw.palette.R
@@ -0,0 +1,28 @@
+#' Convert colors to black and white
+#'
+#' @description
+#' Converts a vector of HEX values to their respective black and white variants, also in HEX.
+#'
+#' @param color
+#'
+#' @export
+
+bnw.palette = function(color){
+  #check if input is valid
+  if(!is.color(color)) stop("invalid color")
+
+  #convert to RGB
+  rgbVals = col2rgb(color)
+
+  #convert to black and white RGB (ie. calculate luminance)
+  bnwVec = apply(rgbVals, 2,
+                 function(x) 0.299 * x[1] + 0.587 * x[2] + 0.114 * x[3]) |>
+            as.integer()
+
+  bnwMatrix = matrix(bnwVec, ncol = length(color), nrow = 3, byrow = TRUE)
+
+  #convert back to HEX
+  bnw = apply(bnwMatrix, 2, function(x) sprintf("#%02X%02X%02X", x[1], x[2], x[3]))
+
+  return(bnw)
+}
